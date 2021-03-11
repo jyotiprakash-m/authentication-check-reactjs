d6c634cd3811c63bd2fba120e34b64c244ea7018
jyotiprakash-m <teachmeonline365@gmail.com>
2021-03-12 00:00:58 +0530

    create signup page

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import Signup from './Signup';
+
+import { Container } from 'react-bootstrap'
 
 function App() {
-  return "Hello world"
+  return (
+    <Container className="d-flex align-items-center justify-content-center" style={{ minWidth: '100%', height: '100vh' }}>
+      <div className="w-100" style={{ maxWidth: "400px" }}>
+        <Signup />
+
+      </div>
+    </Container>
+
+  )
 }
 
 export default App;
