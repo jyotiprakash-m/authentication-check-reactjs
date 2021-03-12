16fc3ad3ca8d729c192b8a5d7f0e2a26b6c623ae
jyotiprakash-m <teachmeonline365@gmail.com>
2021-03-12 15:58:37 +0530

    Setup router to the reaact app

--- src/components/App.js
+++ src/components/App.js
@@ -4,17 +4,28 @@ import Signup from './Signup';
 import { Container } from 'react-bootstrap'
 import AuthProvider from '../contexts/AuthContext';
 
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+
+import Dashboard from './Dashboard'
+import Login from './Login'
 function App() {
   return (
-    <AuthProvider>
-      <Container className="d-flex align-items-center justify-content-center" style={{ minWidth: '100%', height: '100vh' }}>
-        <div className="w-100" style={{ maxWidth: "400px" }}>
-          <Signup />
+    <Container className="d-flex align-items-center justify-content-center" style={{ minWidth: '100%', height: '100vh' }}>
+      <div className="w-100" style={{ maxWidth: "400px" }}>
+        <Router>
+          <AuthProvider>
+            <Switch>
+              <Route exact path="/" component={Dashboard} />
+              <Route path="/signup" component={Signup} />
+              <Route path="/login" component={Login} />
+            </Switch>
+          </AuthProvider>
+        </Router>
 
-        </div>
-      </Container>
 
-    </AuthProvider>
+      </div>
+    </Container>
+
 
 
   )
