f48d50e1b193348cf0cbb5cab612492b119bcdf2
jyotiprakash-m <teachmeonline365@gmail.com>
2021-03-12 15:24:19 +0530

    Add sugnup functionality

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,37 +1,49 @@
-import React, { useRef } from 'react'
-import { Form, Button, Card } from 'react-bootstrap'
+import React, { useRef, useState } from "react"
+import { Form, Button, Card, Alert } from "react-bootstrap"
+import { useAuth } from "../contexts/AuthContext"
 
 export default function Signup() {
     const emailRef = useRef()
     const passwordRef = useRef()
     const passwordConfirmRef = useRef()
+    const { signup } = useAuth()
+
+
+    async function handleSubmit(e) {
+        e.preventDefault()
+
+        await signup(emailRef.current.value, passwordRef.current.value)
+
+
+    }
+
     return (
         <>
             <Card>
                 <Card.Body>
                     <h2 className="text-center mb-4">Sign Up</h2>
-                    <Form>
+                    <Form onSubmit={handleSubmit}>
                         <Form.Group id="email">
                             <Form.Label>Email</Form.Label>
                             <Form.Control type="email" ref={emailRef} required />
                         </Form.Group>
                         <Form.Group id="password">
                             <Form.Label>Password</Form.Label>
-                            <Form.Control type="email" ref={passwordRef} required />
+                            <Form.Control type="password" ref={passwordRef} required />
                         </Form.Group>
                         <Form.Group id="password-confirm">
-                            <Form.Label>Confirm Password</Form.Label>
-                            <Form.Control type="email" ref={passwordConfirmRef} required />
+                            <Form.Label>Password Confirmation</Form.Label>
+                            <Form.Control type="password" ref={passwordConfirmRef} required />
                         </Form.Group>
-
-                        <Button className="w-100" type="submit">Sign Up</Button>
+                        <Button className="w-100" type="submit">
+                            Sign Up
+            </Button>
                     </Form>
                 </Card.Body>
             </Card>
             <div className="w-100 text-center mt-2">
-                Already have an account? Login
+                Already have an account? Log in
             </div>
-
         </>
     )
 }
