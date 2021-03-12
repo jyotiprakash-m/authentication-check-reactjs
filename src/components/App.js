dafc3194101bd6425ce7bd7781c0fe4582d844d0
jyotiprakash-m <teachmeonline365@gmail.com>
2021-03-12 15:24:43 +0530

    modify signup

--- src/components/App.js
+++ src/components/App.js
@@ -2,15 +2,20 @@ import React from 'react';
 import Signup from './Signup';
 
 import { Container } from 'react-bootstrap'
+import AuthProvider from '../contexts/AuthContext';
 
 function App() {
   return (
-    <Container className="d-flex align-items-center justify-content-center" style={{ minWidth: '100%', height: '100vh' }}>
-      <div className="w-100" style={{ maxWidth: "400px" }}>
-        <Signup />
+    <AuthProvider>
+      <Container className="d-flex align-items-center justify-content-center" style={{ minWidth: '100%', height: '100vh' }}>
+        <div className="w-100" style={{ maxWidth: "400px" }}>
+          <Signup />
 
-      </div>
-    </Container>
+        </div>
+      </Container>
+
+    </AuthProvider>
+
 
   )
 }
